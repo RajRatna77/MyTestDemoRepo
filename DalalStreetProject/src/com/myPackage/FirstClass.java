d4696643736d3a5592657b20d6bfbef45226f770
RAJRATNA GAJBHIYE <rajratna0707@gmail.com>
2023-01-05 19:19:35 +0530

    Basic Steps
    Hi Team i have created framework

---
+++ DalalStreetProject/src/com/myPackage/FirstClass.java
@@ -0,0 +1,16 @@
+package com.myPackage;
+
+public class FirstClass 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Added Dependencies");
+		System.out.println("Extent Report");
+		System.out.println("Log4j Report");
+		System.out.println("Basic Stuff");
+		
+	}
+	
+}
